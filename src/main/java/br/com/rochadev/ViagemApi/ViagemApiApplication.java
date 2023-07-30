93f811e70fd25b575ba1d0c025494ded9234478c
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-07-30 15:59:15 -0300

    Estrutura Inicial do Projeto.

---
+++ src/main/java/br/com/rochadev/ViagemApi/ViagemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rochadev.ViagemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViagemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViagemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rochadev/TravelApi/TravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rochadev.TravelApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
